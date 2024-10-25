472974616bf8d8d409f0576f0fe12345fe44ace6
James Ricarte <uhenyou@gmail.com>
2024-10-25 23:27:34 +0800

    trying to connect mongodb but failed

--- server/server.js
+++ server/server.js
@@ -1,50 +1,70 @@
 const express = require('express');
-const Mongoclient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 
 const app = express();
 app.use(cors( {origin: 'http://localhost:5173'} ));
+app.use(express.json());
 
-const CONNECTION_STRING = "";
-const DATABASENAME = "dbname";
+const CONNECTION_STRING = "mongodb+srv://admin:jamesuhenyou123@cluster0.0p7jv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+var DATABASENAME = "dbname";
 let database;
 
-app.listen(5038, () => {
-    // Mongoclient.connect(CONNECTION_STRING, (error, client) => {
-    //     database = client.db(DATABASENAME);
-    //     console.log("Mongo DB Connection Sucessfull");
-    // })
-    console.log("Server started at port 5038");
-})
+// app.listen(5038, () => {
+//     MongoClient.connect(CONNECTION_STRING, (error, client) => {
+//         database = client.db(DATABASENAME);
+//         console.log("Mongo DB Connection Sucessfull");
+//     })
+//     console.log("Server started at port 5038");
+// })
+
+async function startServer() {
+    try {
+        const client = await MongoClient.connect(CONNECTION_STRING, { 
+            connectTimeoutMS: 5000 // Set timeout to 10 seconds
+         });
+        database = client.db(DATABASENAME);
+        console.log("MongoDB Connection Successful");
+
+        app.listen(5038, () => {
+            console.log("Server started at port 5038");
+        });
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        process.exit(1); // Exit the process if the connection fails
+    }
+}
+
+startServer();
 
 //vite and express version:
-app.get('/api', (req, res) => {
-    res.json({fruits: ['apple', 'banana', 'orange', 'lucas']})
-})
+// app.get('/api', (req, res) => {
+//     res.json({fruits: ['apple', 'banana', 'orange', 'lucas']})
+// })
 
 //art of engineer version of mern backend:
-app.get('/api/dbname/getDataV2', (req, res) => {
-    database.collection('dbcollection').find({}).toArray((error, result) => {
-             res.send(result);
-        })
-   })
+// app.get('/api/dbname/getDataV2', (req, res) => {
+//     database.collection('dbcollection').find({}).toArray((error, result) => {
+//              res.send(result);
+//         })
+//    })
    
-   app.post('/api/dbname/addDataV2', (req, res) => {
-    database.collection('dbcollection').count({}, (error, numOfDocs) => {
-    database.collection('dbcollection').insertOne({
-             id: (numOfDocs+1).toString(),
-             description: req.body.addData
-        })
-   res.json('Added Successfully');
-   })
-   })
+//    app.post('/api/dbname/addDataV2', (req, res) => {
+//     database.collection('dbcollection').count({}, (error, numOfDocs) => {
+//     database.collection('dbcollection').insertOne({
+//              id: (numOfDocs+1).toString(),
+//              description: req.body.addData
+//         })
+//    res.json('Added Successfully');
+//    })
+//    })
    
-   app.delete('/api/dbname/deleteDataV2', (req, res) => {
-        database.collection('dbcollection').deleteOne({
-             id: req.query.id
-        })
-        res.json('Delete Successfully');
-   })
+//    app.delete('/api/dbname/deleteDataV2', (req, res) => {
+//         database.collection('dbcollection').deleteOne({
+//              id: req.query.id
+//         })
+//         res.json('Delete Successfully');
+//    })
    
 
 //chatgpt version
